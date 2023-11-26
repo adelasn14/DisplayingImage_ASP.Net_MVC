f2f2c7a66739ad61809fab3d4a815e29f8475bdf
Adela SN <adela.nadhifa14@gmail.com>
2023-11-26 20:02:15 +0700

    initial adding

---
+++ FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult DisplayImage(int Id)
+    {
+        var image = new ImageModel {
+            Id = Id,
+            Name = "Cat touching dandelions",
+            ImagePath = "~/images/cat.jpg"
+        };
+
+        return View(image);
+    }
+
+}
+
---
+++ FirstApp/Models/ImageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApp.Models
+{
+	public class ImageModel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
+
